4a83a975f4f58bd0e0aee0506b79221a99125366
willyweyru2 <willyweyru2@gmail.com>
2026-02-15 19:19:34 +0000

    fix confetti types

--- frontend/types/canvas-confetti.d.ts
+++ frontend/types/canvas-confetti.d.ts
@@ -1,1 +1,17 @@
 declare module "canvas-confetti";
+export interface ConfettiOptions {
+  particleCount?: number;
+  angle?: number;
+  spread?: number;
+  startVelocity?: number;
+  decay?: number;
+  gravity?: number;
+  drift?: number;
+  ticks?: number;
+  zIndex?: number;
+  colors?: string[];
+  shapes?: ("square" | "circle")[];
+  origin?: { x: number; y: number };
+}
+
+export default function confetti(options?: ConfettiOptions): void;
